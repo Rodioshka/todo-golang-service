173bf96305bb52e0bcc3359eb749cf6f6d02ae55
Rodioshka <ZyMamarKiZ34552BeGgi>
2022-08-07 18:21:50 +0300

    add auth

--- pkg/repository/auth_postgres.go
+++ pkg/repository/auth_postgres.go
@@ -23,3 +23,10 @@ func (r *AuthPostgres) CreateUser(user entity.User) (int, error) {
 	}
 	return id, nil
 }
+
+func (s *AuthPostgres) GetUser(username, password string) (entity.User, error) {
+	var user entity.User
+	query := fmt.Sprintf("SELECT id FROM %s WHERE username=$1 AND password_hash=$2", userTable)
+	err := s.db.Get(&user, query, username, password)
+	return user, err
+}
--- pkg/service/auth.go
+++ pkg/service/auth.go
@@ -3,14 +3,24 @@ package service
 import (
 	"crypto/sha1"
 	"fmt"
+	"github.com/dgrijalva/jwt-go"
+	"os"
+	"time"
 	"todo-app/pkg/entity"
 	"todo-app/pkg/repository"
 )
 
-const salt = "h4k25345ldfksdfl53"
+const (
+	tokenTTL = 12 * time.Hour
+)
 
 type AuthService struct {
 	repo repository.Authorization
+}
+
+type tokenClaims struct {
+	jwt.StandardClaims
+	UserId int `json:"user_id"`
 }
 
 func NewAuthService(repo repository.Authorization) *AuthService {
@@ -27,5 +37,22 @@ func (s *AuthService) CreateUser(user entity.User) (int, error) {
 func generatePasswordHash(password string) string {
 	hash := sha1.New()
 	hash.Write([]byte(password))
-	return fmt.Sprintf("%x", hash.Sum([]byte(salt)))
+	return fmt.Sprintf("%x", hash.Sum([]byte(os.Getenv("salt"))))
+}
+
+func (s *AuthService) GenerateToken(username string, password string) (string, error) {
+	user, err := s.repo.GetUser(username, generatePasswordHash(password))
+	if err != nil {
+		return "", err
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, &tokenClaims{
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(tokenTTL).Unix(),
+			IssuedAt:  time.Now().Unix(),
+		},
+		user.Id,
+	})
+
+	return token.SignedString([]byte(os.Getenv("signInKey")))
 }
